6302ffca8f8be9d19c4fcd7f3005863c59771d57
Kacangturbo <76491235+Kacangturbo@users.noreply.github.com>
2020-12-27 23:13:46 +0700

    Add files via upload

---
+++ kuis.py
@@ -0,0 +1,15 @@
+#python 2.7.15
+
+
+print "masukan bilangan aritmatika"
+nilai = 12
+score = 0
+print ("score:") +str(score)
+print "12 +... = 24"
+Say =input("Masukan jawaban: ") 
+if Say == 12 :
+  nilai += 12
+  score += 5
+  print (nilai)
+  print "score " +str(score)
+  print "Benar" 
